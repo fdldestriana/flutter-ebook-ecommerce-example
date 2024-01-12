45523a3849362d53b70dd2a178836d76acc9e945
fadlydestriana <fadlydestriana@gmail.com>
2024-01-12 22:17:12 +0700

    layouting the grid product

--- lib/views/product_list_view.dart
+++ lib/views/product_list_view.dart
@@ -1,5 +1,4 @@
 import 'package:ecommerce_example/data/source/product_list.dart';
-import 'package:ecommerce_example/state_util.dart';
 import 'package:ecommerce_example/widgets/re_product_item.dart';
 import 'package:flutter/material.dart';
 
@@ -9,15 +8,23 @@ class ProductListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.separated(
-        separatorBuilder: (context, index) => SizedBox(
-          width: Get.width * 0.05,
-        ),
-        shrinkWrap: true,
-        itemCount: productList.length,
-        scrollDirection: Axis.vertical,
-        itemBuilder: (context, index) => ReProductItem(
-          product: productList[index],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: GridView.builder(
+            shrinkWrap: true,
+            itemCount: productList.length,
+            scrollDirection: Axis.vertical,
+            itemBuilder: (context, index) => ReProductItem(
+              product: productList[index],
+            ),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              childAspectRatio: 150 / 260,
+              crossAxisCount: 2,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 26,
+            ),
+          ),
         ),
       ),
     );
